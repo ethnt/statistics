70122412bb146a44d876867502fccc68066e453a
Ivar Vasara <ivar@oobik.com>
2013-11-15 06:30:06 -0800

    minor refactoring

--- lib/statistics/standardization.rb
+++ lib/statistics/standardization.rb
@@ -3,7 +3,7 @@ module Statistics
     extend Statistics::Quantitative
 
     def self.z_score(x, mean, std_dev)
-      return ((x.to_f - mean.to_f) / std_dev.to_f).to_f
+      (x.to_f - mean.to_f) / std_dev.to_f
     end
   end
 end
